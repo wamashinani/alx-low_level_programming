fdce85340f9b1928ecc46e9324add967963b1148
root <siele137@gmail.com>
2022-09-09 04:40:13 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+/**
+*main -print the string in the put function
+*
+*Description;using the main function
+*this program prints 'programming is like building a puzzle
+*return;0
+*/
+int main(void)
+{
+puts("\"programming is like building a puzzle");
+return(0);
+}
