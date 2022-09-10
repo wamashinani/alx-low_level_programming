2be2dbf54e45014d4e0030cdb979b8df21f2b52e
root <siele137@gmail.com>
2022-09-09 22:46:28 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -9,13 +9,9 @@
 int main(void)
 {
 char c;
-for (c = '0'; c <= '9'; c++)
+for (c = 'z'; c >= 'a'; c--)
 {
-	putchar(c);
-}
-for (c = 'a'; c <= 'f'; c++)
-{
-	putchar(c);
+putchar(c);
 }
 putchar('\n');
 return (0);
