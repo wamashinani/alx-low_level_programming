b0351e3490738da635668d5064e6d28a79a18b68
root <siele137@gmail.com>
2022-09-09 22:59:58 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,10 +11,11 @@ int main(void)
 char ch;
 for (ch = 'a' ; ch <= 'z' ; ch++)
 {
-if (ch != 'e' && ch != 'q')
-{
 	putchar(ch);
 }
+for (ch = 'A' ; ch <= 'Z' ; ch++)
+{
+	putchar(ch);
 }
 putchar('\n');
 return (0);
