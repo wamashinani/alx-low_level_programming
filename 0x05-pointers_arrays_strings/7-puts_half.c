13d4230d9d6084ce460fe78bb0c48e18ddf9f804
root <siele137@gmail.com>
2022-09-19 11:20:46 -0700

    check

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,16 +1,13 @@
 #include "main.h"
 
-
 /**
  * puts_half - Print half a str
  * @str: a printer
  */
 
-
 void puts_half(char *str)
- {
-
- 	 int index = 0, len = 0, n;
+{
+	int index = 0, len = 0, n;
 
 	while (str[index++])
 		len++;
@@ -24,5 +21,5 @@ void puts_half(char *str)
 		_putchar(str[index]);
 
 	_putchar('\n');
- }
+}
 
