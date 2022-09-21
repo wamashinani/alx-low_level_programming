11eb75c8ca6687112dff9ed6c32bc11cb11c622a
root <siele137@gmail.com>
2022-09-21 08:01:28 -0700

    task 1

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+#Include <stdio.h>
+/**
+ * void prototype()
+ * int prototype()
+ * chair prototypes()
+ */
+void reverse_array(int *a,int n);
+void print_number(int n);
+void print_buffer(char *,int size);
+int _strcmp(char *s2);
+char *_strncat(char *dest,char *src);
+char *-strncat(char *dest,char *src,int n);
+char *string_toupper(char*);
+char *cap_string(char*);
+char *cap_string(char*);
+char *leet(char*);
+char *rot13(char*);
+char *infinite_add(char *n1,char *r,int size_r);
+char *add_string(char *n1,char *r,int r_index);
+#endif/*MAIN_H*/
+:set number
+
