1e1cb59113010bf5041a220ccfe4d8d06ec494c0
root <siele137@gmail.com>
2022-09-21 11:53:03 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,43 +1,31 @@
 #include "main.h"
 
-
-
-
-
-
-
-
-
 /**
+ * *_strcat - function commute srtings
+ * @dest: param pointer to a char
+ * @src: param pointer to a char
+ * Return: return value of dest
+ */
 
-
-* reset_to_98 - ur comment
-
-
-* @n: pointer
-
-
-* Return: void
-
-
-*/
-
-
-
-
-
-
-
-void reset_to_98(int *n)
-
-
+char *_strcat(char *dest, char *src)
 {
+	int i;
+	int j;
 
+	i = 0;
+	j = 0;
 
-	*n = 98;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
 
-
+	dest[i] = '\0';
+	return (dest);
 }
-
-
-
