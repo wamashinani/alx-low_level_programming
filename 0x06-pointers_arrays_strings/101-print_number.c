73613d10a2689c62dd32881d0531f114f044b9c6
root <siele137@gmail.com>
2022-09-21 22:02:51 -0700

    checked

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -36,9 +36,3 @@ char *cap_string(char *str)
                                                                                                                                       
         return (str);                                                                                                                 
 }
-
-
-
-
-
-
