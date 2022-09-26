f1fc7144ff70c5f42dd252f25da7e8715852691b
root <siele137@gmail.com>
2022-09-26 06:39:33 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,28 +1,24 @@
-/*                                                                                                                                   
- * File: 0-memset.c                                                                                                                  
- * Auth: wamashinani                                                                                                                         
- */                                                                                                                                  
-                                                                                                                                     
-#include "main.h"                                                                                                                    
-                                                                                                                                     
-/**                                                                                                                                  
- * _memset - Fills the first n bytes of the memory area                                                                              
- *          pointed to by @s with the constant byte @c.                                                                              
- * @s: A pointer to the memory area to be filled.                                                                                    
- * @c: The character to fill the memory area with.                                                                                   
- * @n: The number of bytes to be filled.                                                                                             
- * description _memset: over there                                                                                                   
- *                                                                                                                                   
- * Return: A pointer to the filled memory area @s.                                                                                   
- *                                                                                                                                   
- */                                                                                                                                  
-char *_memset(char *s, char b, unsigned int n)                                                                                              
-{                                                                                                                                    
-        while (i< n)  
-	{
+#include "main.h"
 
-            *(s + i) = b;
-	    i++;
-	}   
-                                                                                   return (s);                                                                                                             
+/**
+ *_strcat - concatenates  the string pointed to by @src to
+ * the end of the string pointed to by @dest
+ *@dest: String that will be appended
+ *@src: String to be concatenated upon
+ *
+ * Return: returns poiner to @dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
 }
