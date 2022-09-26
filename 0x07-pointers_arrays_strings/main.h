b383055f543caefbdf469adefb7289ebe15d6688
root <siele137@gmail.com>
2022-09-26 05:53:53 -0700

    kindly check

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,16 +2,13 @@
 #define MAIN_H                                                                                                                     
                                                                                                                                    
 /**                                                                                                                                
- * File: main.h                                                                                                                    
- * Auth: sam tech (samuel birhanu)                                                                                                 
+ * File: main.h            
+ *
  * description: Header file containing prototypes for all functions                                                                
- *       used in the 0x06-pointers_arrays_strings directory.                                                                       
- */                                                                                                                                
-                                                                                                                                   
-#include <stddef.h>                                                                                                                
-                                                                                                                                   
-int _putchar(char c);                                                                                                              
-void *_memset(void *s, int c, size_t n);                                                                                           
+ *       used in the 0x07-pointers_arrays_strings directory.                                                                       
+ */                                                                                                                                    
+int _putchar(char);                                                                                                              
+void *_memset(char *s, char b, unsigned int n);                                                                                           
 char *_memcpy(char *dest, char *src, unsigned int n);                                                                              
 char *_strchr(char *s, char c);                                                                                                    
 unsigned int _strspn(char *s, char *accept);                                                                                       
@@ -21,5 +18,5 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);                                                                                             
 void set_string(char **s, char *to);                                                                                               
                                                                                                                                    
-#endif
+#endif/*MAIN_H*/
 
