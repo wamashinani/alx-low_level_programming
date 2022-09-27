5ec3a9523184f523f30117dbf2c7a80f7b379573
root <siele137@gmail.com>
2022-09-27 07:55:26 -0700

    checked advanced

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,24 @@
+/*                                                                                                                                 
+ * File: 100-set_string.c                                                                                                        
+ * Auth: wamashinani                                                                                                                  
+ */                                                                                                                                
+                                                                                                                                   
+#include "main.h"                                                                                                                  
+#include <stdio.h>                                                                                                                 
+                                                                                                                                   
+/**                                                                                                                                
+ * print_diagsums - Prints the sum of the two diagonals                                                                            
+ *                  of a square matrix of integers.                                                                                
+ * @a: The matrix of integers.                                                                                                     
+ * @size: The size of the matrix.                                                                                                  
+ */
+void print_diagsums(int *a, int size)                                                                                              
+{                                                                                                                                  
+        int index, sum1 = 0, sum2 = 0;                                                                                             
+                                                                                                                                   
+        for (index = 0; index < size; index++)                                                                                     
+        {                                                                                                                          
+                sum1 += a[index];                                                                                                  
+                a += size;                                                                                                         
+        }                             
+}
