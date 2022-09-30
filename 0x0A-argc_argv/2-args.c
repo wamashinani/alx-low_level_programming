0790b52544d496402786e8bbbc6e3a5016991ac5
root <siele137@gmail.com>
2022-09-30 03:59:35 -0700

    task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,17 +2,22 @@
 #include "main.h"                                                                                                                    
                                                                                                                                      
 /**                                                                                                                                  
- * main - print the name of the program                                                                                              
+ * main - Print the name of the program                                                                                              
  * @argc: Count arguments                                                                                                            
-i * @argv: Arguments                                                                                                                  
+ * @argv: Arguments                                                                                                                  
  *                                                                                                                                   
  * Return: Always 0 (Success)                                                                                                        
  */                                                                                                                                  
-                                                                                      int main(int argc, char *argv[])                                                                                                     
-{                                                                                                                                    
-(void) argv; /*Ignore argv*/                                                                                                         
-printf("%i\n", argc - 1);                                                                                                            
                                                                                                                                      
-return (0);                                                                                                                          
-}
-
+int main(int argc, char *argv[])                                                                                                     
+{                                                                                                                                    
+                                                                                                                                     
+/*Declaring variables*/                                                                                                              
+int count = 0;                                                                                                                       
+                                                                                                                                     
+if (argc > 0)                                                                                                                        
+{                                                                                                                                    
+/*WHILE - Print each arguments*/                                                                                                     
+while (count < argc)                                                                                                                 
+{                                                                                                                                    
+printf("%s\n", argv[count]);
