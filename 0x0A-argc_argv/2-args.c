e2410365005e17d5313d8bca4a4c633203e34547
root <siele137@gmail.com>
2022-10-01 04:23:20 -0700

    check

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
          int n;                                                                                                                      
          for(n =0; n < argc; n++)                                                                                                                        
          {                                                                                                                 
-                 printf("%s\n",argc)
+                 printf("%s\n",argv[n]);
       		 
          }                                                                               return(0);
 }	 
