6e0aea6137f1b1715aff3226106bdc545db3a91f
root <siele137@gmail.com>
2022-10-01 00:24:45 -0700

    libmy.a, main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,12 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-/*
- *  * File: main.h
- *   * Auth: wamashinani
- *    * Desc: Header file containing the prototypes of all functions
- *     *       included in the static library libholberton.a.
- *      */
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -28,4 +21,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif 
