03022a188cd473b31536d2bf3ea2c18cb56191f7
root <siele137@gmail.com>
2022-10-06 09:11:12 -0700

    check task o

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdlib.h>
 
 /**
@@ -9,11 +8,10 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	void *mem = malloc(b);
+	void *p = malloc(b);
 
-	if (mem == NULL)
+	if (p == NULL)
 		exit(98);
 
-	return (mem);
+	return (p);
 }
-
