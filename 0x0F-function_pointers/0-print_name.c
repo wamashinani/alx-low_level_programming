59b9a92477afa3a82dbde13ca4dddfba258c32b6
root <siele137@gmail.com>
2022-10-13 06:52:48 -0700

    check

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
-#include <stdarg.h>
-#include "variadic_functions.h"
+#include "function_pointers.h"
 
 /**
  * print_name - prints a name.
@@ -8,15 +7,8 @@
  *
  * Return: no return.
  */
-int sum_them_all(const unsigned int n, ...)
+void print_name(char *name, void (*f)(char *))
 {
-        int sum = 0;
-        unsigned int i;
-        va_list vaist;
-
-        va_start(valist, n);
-        for (i = 0; i < n; i++)
-                 sum += va_arg(valist, int);
-        va_end(valist);
-        return (sum);
+	if (name && f)
+		f(name);
 }
