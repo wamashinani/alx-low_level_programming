dcde6f909405cfd273384cf7321e00524c78e128
root <siele137@gmail.com>
2022-12-13 06:06:29 -0800

    checked

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef " main.h"
-#define " main.h"
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _putchar(char c);
 int _islower(int c);
@@ -9,7 +9,8 @@ int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
 void _puts(char *s);
-char *_strcpy(char *dest, char *src)atoi(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -21,4 +22,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endi */  main.h */
+#endif /* _MAIN_H_ */
